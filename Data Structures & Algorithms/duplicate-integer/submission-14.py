57e34d3e410cf867878d12bbc3450ba61cadc6c4
Mihir Jani <138828353+mihirzx@users.noreply.github.com>
2026-05-30 20:40:04 -0400

    Add: duplicate-integer - submission-14

---
+++ Data Structures & Algorithms/duplicate-integer/submission-14.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        countMap = {}
+        for num in nums:
+            if num not in countMap:
+                countMap[num] = 1
+            else:
+                return True 
+        return False
